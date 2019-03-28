55b3632ed0bc6745670d2aa50c19667c2cf09c75
Luis Weich <mambabosso@gmail.com>
2019-03-28 14:30:41 +0100

    Add files via upload

--- src/eznetworking/util/Progress.java
+++ src/eznetworking/util/Progress.java
@@ -2,18 +2,52 @@ package eznetworking.util;
 
 import java.util.ArrayList;
 
-public class Progress<T> {
+public class Progress<T extends Number> {
 
-    public interface ProgressChanged<T> {
+    public interface ProgressStarted<T extends Number> {
+        public void started(Progress<T> sender, T value);
+    }
+
+    public interface ProgressChanged<T extends Number> {
         public void changed(Progress<T> sender, T value);
     }
 
+    public interface ProgressFinished<T extends Number> {
+        public void finished(Progress<T> sender, T value);
+    }
+
+    private ArrayList<ProgressStarted<T>> progressStartedEvents = new ArrayList<>();
     private ArrayList<ProgressChanged<T>> progressChangedEvents = new ArrayList<>();
+    private ArrayList<ProgressFinished<T>> progressFinishedEvents = new ArrayList<>();
+
+    private void triggerProgressStarted(T value) {
+        Runner.run(() -> {
+            for (ProgressStarted<T> ps : progressStartedEvents) {
+                ps.started(this, value);
+            }
+        });
+    }
+
+    public void addProgressStartedListener(ProgressStarted<T> listener) {
+        if (listener == null) {
+            throw new IllegalArgumentException();
+        }
+        progressStartedEvents.add(listener);
+    }
+
+    public boolean removeProgressStartedListener(ProgressStarted<T> listener) {
+        if (listener == null) {
+            throw new IllegalArgumentException();
+        }
+        return progressStartedEvents.remove(listener);
+    }
 
     private void triggerProgressChanged(T value) {
-        for (ProgressChanged<T> pg : progressChangedEvents) {
-            pg.changed(this, value);
-        }
+        Runner.run(() -> {
+            for (ProgressChanged<T> pg : progressChangedEvents) {
+                pg.changed(this, value);
+            }
+        });
     }
 
     public void addProgressChangedListener(ProgressChanged<T> listener) {
@@ -30,7 +64,38 @@ public class Progress<T> {
         return progressChangedEvents.remove(listener);
     }
 
-    public void report(T value) {
+    private void triggerProgressFinished(T value) {
+        Runner.run(() -> {
+            for (ProgressFinished<T> pf : progressFinishedEvents) {
+                pf.finished(this, value);
+            }
+        });
+    }
+
+    public void addProgressFinishedListener(ProgressFinished<T> listener) {
+        if (listener == null) {
+            throw new IllegalArgumentException();
+        }
+        progressFinishedEvents.add(listener);
+    }
+
+    public boolean removeProgressFinishedListener(ProgressFinished<T> listener) {
+        if (listener == null) {
+            throw new IllegalArgumentException();
+        }
+        return progressFinishedEvents.remove(listener);
+    }
+
+    public void started(T value) {
+        triggerProgressStarted(value);
+    }
+
+    public void changed(T value) {
         triggerProgressChanged(value);
     }
+
+    public void finished(T value) {
+        triggerProgressFinished(value);
+    }
+
 }
