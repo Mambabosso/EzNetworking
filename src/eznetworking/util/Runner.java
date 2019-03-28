326544f8ba146eb9c7b48661e04f72e5034d807b
Luis Weich <mambabosso@gmail.com>
2019-03-28 15:31:37 +0100

    Add files via upload

--- src/eznetworking/util/Runner.java
+++ src/eznetworking/util/Runner.java
@@ -2,14 +2,16 @@ package eznetworking.util;
 
 public final class Runner {
 
+    public static void run(Runnable r, boolean newThread) {
+        if (newThread) {
+            new Thread(() -> r.run()).start();
+        } else {
+            r.run();
+        }
+    }
+
     public static void run(Runnable r) {
-        Thread t = new Thread(() -> {
-            try {
-                r.run();
-            } catch (Exception ex) {
-            }
-        });
-        t.start();
+        run(r, false);
     }
 
 }
