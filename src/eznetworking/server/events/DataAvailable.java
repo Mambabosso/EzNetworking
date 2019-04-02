64e9c409293e1f33edd336d320266128f706b5d9
Luis Weich <mambabosso@gmail.com>
2019-04-02 20:46:47 +0200

    Add files via upload

---
+++ src/eznetworking/server/events/DataAvailable.java
@@ -0,0 +1,9 @@
+package eznetworking.server.events;
+
+import eznetworking.server.connection.Connection;
+import eznetworking.server.Server;
+import eznetworking.util.Progress;
+
+public interface DataAvailable {
+    public void available(Server sender, Connection client, int type, int length, Progress<Integer> progress);
+}
