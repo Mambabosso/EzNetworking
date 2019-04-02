8e67d4c5ee3a4aaa2938e2e38ca447165b8a7167
Luis Weich <mambabosso@gmail.com>
2019-04-02 20:44:43 +0200

    Delete NewDataAvailable.java

--- src/eznetworking/server/events/NewDataAvailable.java
+++
@@ -1,9 +0,0 @@
-package eznetworking.server.events;
-
-import eznetworking.server.connection.Connection;
-import eznetworking.server.Server;
-import eznetworking.util.Progress;
-
-public interface NewDataAvailable {
-    public void available(Server sender, Connection client, int type, int length, Progress<Integer> progress);
-}
